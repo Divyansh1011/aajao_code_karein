146b9eadd803495883f79882b4b46eb499d811a3
Divyansh Agarwal <divyanshagarwal80@gmail.com>
2021-09-02 00:56:04 +0530

    learning java_day_1

---
+++ main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class main 
+{
+    public static void main (String[] args)
+    { 
+        Scanner input  = new Scanner(System.in);
+        //taking the input 
+    
+        System.out.println(input.nextInt());
+
+
+        //diffrent datatypes with the methods 
+
+        Integer helo = 64;
+        helo.toString();
+        
+    }
+  
+    
+}
---
+++ test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        float sum = input.nextFloat();
+        System.out.println(sum);
+
+        //type casting  
+
+        int man = (int)(input.nextFloat());
+        System.out.println(man);
+
+
+
+        
+
+    }
+    
+}
