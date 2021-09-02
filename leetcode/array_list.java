3c981843866a91962ad2de21137d9219fdf1b2ff
Divyansh Agarwal <divyanshagarwal80@gmail.com>
2021-09-02 22:40:40 +0530

    ordered_the_work largest_permutation

---
+++ leetcode/array_list.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class array_list {
+    public static void main(String[] args) {
+
+        Scanner n = new Scanner(System.in);
+        ArrayList<Integer> b = new ArrayList<Integer>();
+
+        for(int i = 0;i<b.size();i++)
+        {
+            b.add(n.nextInt());
+        }
+    }
+}
+
