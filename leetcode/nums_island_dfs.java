ffcdba6a4d0565f7f9cbf300807546e9bdec8691
Divyansh Agarwal <divyanshagarwal80@gmail.com>
2022-07-01 17:56:45 +0700

    leetcode-7-graph

---
+++ leetcode/nums_island_dfs.java
@@ -0,0 +1,49 @@
+public class nums_island_dfs 
+{
+    public int numIslands(char[][] grid) 
+    {
+        boolean visited[][] = new boolean[grid.length][grid[0].length];
+        int count = 0;
+        for(int row = 0;row<grid.length;row++)
+        {
+            for(int column = 0;column<grid[0].length;column ++)
+            {
+                    if(visited[row][column] == false && grid[row][column] == '1')
+                    {
+                        count++;
+                        traverse(grid,visited,row,column);
+                        
+                    }
+            }
+        }
+        return count;
+        
+    }
+    public void traverse(char[][] grid, boolean visited[][],int row,int column )
+    {
+        if(row<0 || column <0 || row>= grid.length || column >= grid[0].length)
+        {
+            return ;
+        }
+        else if(visited[row][column] == true )
+        {
+            return;
+        }
+        else if(grid[row][column] == '0')
+        {
+            return; 
+            
+        }
+        
+        visited[row][column] = true;
+        traverse(grid,visited,row+1,column);
+        traverse(grid,visited,row-1,column);
+        traverse(grid,visited,row,column + 1);
+        traverse(grid,visited,row,column -1);
+    }
+            
+    
+    public static void main(String[] args) {
+        
+    }
+}
