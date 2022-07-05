e9377afc4f00e61d41311842920acb92ea40fdfa
Divyansh Agarwal <divyanshagarwal80@gmail.com>
2022-07-05 20:27:03 +0700

    leetcode-dp

---
+++ leetcode/fibonacci_number_dp_1.java
@@ -0,0 +1,24 @@
+public class fibonacci_number_dp_1 {
+    public int climbStairs(int n) 
+    {
+        if(n==1 ||  n==2)
+        {
+            return n;
+        }
+ int a = 1;
+        int b = 2;
+        int c = 0;
+        
+        for(int i = 3;i<=n;i++)
+        {
+            c = a+b;
+            a= b;
+            b=c;
+        }
+        return c;
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ leetcode/min_cost_climbing_stairs_dp_3.java
@@ -0,0 +1,24 @@
+public class min_cost_climbing_stairs_dp_3 {
+    public int minCostClimbingStairs(int[] cost) 
+    {
+        int n = cost.length;
+        int dp[] = new int[n+1];
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i = 2;i<n;i++)
+        {
+            dp[i] = cost[i] + Math.min(dp[i-1] ,dp[i-2]);
+            
+            
+        }
+        
+  return Math.min(dp[n-1], dp[n-2]);
+        
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
